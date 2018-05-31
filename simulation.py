8d0162c245c38ab2a8ecb70e583709676b98f7fe
Siyu Zhou <siyu.zhou@outlook.com>
2018-05-31 12:53:59 -0700

    Restructure.

--- simulation.py
+++ simulation.py
@@ -1,13 +1,47 @@
 import argparse
 import numpy as np
 
-
 from classes import *
 
 
-def main(args):
-    env = Environment2D((-100, 100, -100, 100))
-    for _ in range(args.agents):
+def animate(env, region):
+    import matplotlib.pyplot as plt
+    from matplotlib import animation
+
+    plt.rcParams['animation.html'] = 'html5'
+
+    def animate(i, scat, env):
+        env.update(ARGS.dt)
+
+        scat.set_offsets([boid.position for boid in env.population])
+        return scat,
+
+    xmin, xmax, ymin, ymax = region
+
+    fig, ax = plt.subplots()
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+    ax.set_aspect('equal')
+
+    scat = ax.scatter([], [])
+
+    for goal in env.goals:
+        ax.scatter(*goal.position, color='g')
+    for obstacle in env.obstacles:
+        if not isinstance(obstacle, Wall):
+            ax.scatter(*obstacle.position, color='r')
+
+    anim = animation.FuncAnimation(fig, animate,
+                                   fargs=(scat, env),
+                                   frames=ARGS.steps, interval=20, blit=True)
+
+    anim.save(ARGS.save_name+'.gif', dpi=80, writer='imagemagick')
+
+
+def main():
+    region = (-100, 100, -100, 100)
+    env = Environment2D(region)
+    for _ in range(ARGS.agents):
         env.add_agent(Boid.random(
             100, 15, comfort_zone=3, speed_cap=15, ndim=2))
 
@@ -17,39 +51,14 @@ def main(args):
     sphere = Sphere(np.random.uniform(-20, 20, 2) + goal.position, ndim=2)
     env.add_obstacle(sphere)
 
-    if args.animation:  # Generate animation
-        import matplotlib.pyplot as plt
-        from matplotlib import animation
-
-        plt.rcParams['animation.html'] = 'html5'
-
-        def animate(i, scat, env):
-            env.update(args.dt)
-
-            scat.set_offsets([boid.position for boid in env.population])
-            return scat,
-
-        fig, ax = plt.subplots()
-        ax.set_xlim(-100, 100)
-        ax.set_ylim(-100, 100)
-        ax.set_aspect('equal')
-
-        scat = ax.scatter([], [])
-        ax.scatter(*goal.position)
-        ax.scatter(*sphere.position)
-
-        anim = animation.FuncAnimation(fig, animate,
-                                       fargs=(scat, env),
-                                       frames=args.steps, interval=20, blit=True)
-
-        anim.save(args.save_name+'.gif', dpi=80, writer='imagemagick')
-
+    if ARGS.animation:  # Generate animation
+        animate(env, region)
     else:  # Generate data
         position_data_all = []
         velocity_data_all = []
         for i in range(args.instances):
             if i % 100 == 0:
-                print('Simulation {}/{}...'.format(i, args.instances))
+                print('Simulation {}/{}...'.format(i, ARGS.instances))
 
             position_data = []
             velocity_data = []
@@ -68,7 +77,7 @@ def main(args):
         position_data_all = np.asarray(position_data_all).transpose(0, 1, 3, 2)
         velocity_data_all = np.asarray(velocity_data_all).transpose(0, 1, 3, 2)
 
-        print('All {} simulations completed.'.format(args.instances))
+        print('All {} simulations completed.'.format(ARGS.instances))
 
         np.save('data/'+args.save_name+'_position.npy', position_data_all)
         np.save('data/'+args.save_name+'_velocity.npy', velocity_data_all)
@@ -82,13 +91,13 @@ if __name__ == '__main__':
                         help='number of simulation steps')
     parser.add_argument('--instances', type=int, default=1,
                         help='number of simulation instances')
-    parser.add_argument('--dt', type=float, default=0.03,
+    parser.add_argument('--dt', type=float, default=0.1,
                         help='time resolution')
     parser.add_argument('--animation', action='store_true',
                         help='whether animation is generated')
     parser.add_argument('--save-name', type=str,
                         help='name of the save file')
 
-    args = parser.parse_args()
+    ARGS = parser.parse_args()
 
-    main(args)
+    main()
