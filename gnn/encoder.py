e406f6d24a0b4279b6bb11a5ed1d5c71a037293d
Siyu Zhou <siyu.zhou@outlook.com>
2018-06-26 16:48:57 -0700

    Add refactor.

--- gnn/encoder.py
+++ gnn/encoder.py
@@ -26,19 +26,41 @@ def mlp_encoder(features, classes, params, training=False):
     edge_sources = tf.one_hot(edge_sources, num_agents)
     edge_targets = tf.one_hot(edge_targets, num_agents)
 
-    msg_from_source = tf.transpose(tf.tensordot(state, edge_sources, axes=[[1], [1]]),
-                                   perm=[0, 2, 1])
-    msg_from_target = tf.transpose(tf.tensordot(state, edge_targets, axes=[[1], [1]]),
-                                   perm=[0, 2, 1])
-    ## msg_source and msg_target in shape [num_sims, num_edges, out_units]
-    msg_edge = tf.concat([msg_from_source, msg_from_target], axis=-1)
+    def node_to_edge(node_state):
+        """Propagate node states to edges."""
+        msg_from_source = tf.transpose(tf.tensordot(state, edge_sources, axes=[[1], [1]]),
+                                       perm=[0, 2, 1])
+        msg_from_target = tf.transpose(tf.tensordot(state, edge_targets, axes=[[1], [1]]),
+                                       perm=[0, 2, 1])
+        ## msg_from_source and msg_from_target in shape [num_sims, num_edges, out_units]
+        msg_edge = tf.concat([msg_from_source, msg_from_target], axis=-1)
 
-    # Encode edge messages with MLP
-    msg_edge = mlp_layers(msg_edge,
-                          params['hidden_units'],
-                          params['dropout'],
-                          params['batch_norm'],
-                          training=training)
+        # Encode edge messages with MLP
+        msg_edge = mlp_layers(msg_edge,
+                              params['hidden_units'],
+                              params['dropout'],
+                              params['batch_norm'],
+                              training=training)
+        return msg_edge
+
+    msg_edge = node_to_edge(state)
+
+    def edge_to_node(edge_msg):
+        """Send edge messages to target nodes."""
+        node_state = tf.transpose(tf.tensordot(edge_msg, edge_targets, axes=[[1], [0]]),
+                                  perm=[0, 2, 1])  # Shape [num_sims, num_agents, out_units].
+        # Encode node state with MLP
+        node_state = mlp_layers(node_state,
+                                params['hidden_units'],
+                                params['dropout'],
+                                params['batch_norm'],
+                                training=training)
+        return node_state
+
+    state = edge_to_node(msg_edge)
+
+    # Propagate node states to edges again.
+    msg_edge = node_to_edge(state)
 
     edge_type = tf.layers.dense(msg_edge, classes)
 
