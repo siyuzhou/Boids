0cdcf34bfa878c7757265a62b81c9cabdc065958
Siyu Zhou <siyu.zhou@outlook.com>
2018-07-13 10:25:22 -0700

    Group ops and vars in scopes.

--- gnn/decoder.py
+++ gnn/decoder.py
@@ -16,25 +16,28 @@ def mlp_decoder(features,  params, training=False):
     edge_sources, edge_targets = np.where(fc_matrix(num_agents))
     # One-hot representation of indices of edge sources and targets.
     # `edge_sources` and `edge_targets` in shape [num_edges, num_agents].
-    edge_sources = tf.one_hot(edge_sources, num_agents)
-    edge_targets = tf.one_hot(edge_targets, num_agents)
+    with tf.name_scope('one_hot'):
+        edge_sources = tf.one_hot(edge_sources, num_agents)
+        edge_targets = tf.one_hot(edge_targets, num_agents)
 
-    msg_from_source = tf.transpose(tf.tensordot(time_series, edge_sources, axes=[[2], [1]]),
-                                   perm=[0, 1, 3, 2])
-    msg_from_target = tf.transpose(tf.tensordot(time_series, edge_targets, axes=[[2], [1]]),
-                                   perm=[0, 1, 3, 2])
-    # msg_from_source and msg_from_target in shape [num_sims, time_steps, num_edges, ndims]
-    msg_edge = tf.concat([msg_from_source, msg_from_target], axis=-1)
+    with tf.name_scope('node_to_edge'):
+        msg_from_source = tf.transpose(tf.tensordot(time_series, edge_sources, axes=[[2], [1]]),
+                                       perm=[0, 1, 3, 2])
+        msg_from_target = tf.transpose(tf.tensordot(time_series, edge_targets, axes=[[2], [1]]),
+                                       perm=[0, 1, 3, 2])
+        # msg_from_source and msg_from_target in shape [num_sims, time_steps, num_edges, ndims]
+        msg_edge = tf.concat([msg_from_source, msg_from_target], axis=-1)
 
     encoded_msg_by_type = []
     # Encode edge message by types and concatenate them.
-    for _ in range(num_types):
+    for i in range(num_types):
         # mlp_encoder for one edge type.
         encoded_msg = mlp_layers(msg_edge,
                                  params['hidden_units'],
                                  params['dropout'],
                                  params['batch_norm'],
-                                 training=training)
+                                 training=training,
+                                 name='edge_MLP_encoder_{}'.format(i))
 
         encoded_msg_by_type.append(encoded_msg)
 
@@ -43,32 +46,35 @@ def mlp_decoder(features,  params, training=False):
     # shape [num_sims, time_steps, num_edges, num_types, out_units]
 
     # Expand edge_type shape to [num_sims, 1, num_edges, num_edge_types, 1]
-    edge_type = tf.expand_dims(edge_type, 1)
-    edge_type = tf.expand_dims(edge_type, 4)
+    with tf.name_scope('edge_encoding_avg'):
+        edge_type = tf.expand_dims(edge_type, 1)
+        edge_type = tf.expand_dims(edge_type, 4)
 
-    # Sum of the edge encoding from all possible types.
-    encoded_msg_sum = tf.reduce_sum(tf.multiply(encoded_msg_by_type,
-                                                edge_type),
-                                    axis=3)
-    # shape [num_sims, time_steps, num_edges, out_units]
+        # Sum of the edge encoding from all possible types.
+        encoded_msg_sum = tf.reduce_sum(tf.multiply(encoded_msg_by_type,
+                                                    edge_type),
+                                        axis=3)
+        # shape [num_sims, time_steps, num_edges, out_units]
 
     # Aggregate msg from all edges to target node.
-    msg_aggregated = tf.transpose(tf.tensordot(encoded_msg_sum, edge_targets, axes=[[2], [0]]),
-                                  perm=[0, 1, 3, 2])
-    # shape [num_sims, time_steps, num_agents, out_units]
+    with tf.name_scope('edge_to_node'):
+        msg_aggregated = tf.transpose(tf.tensordot(encoded_msg_sum, edge_targets, axes=[[2], [0]]),
+                                      perm=[0, 1, 3, 2])
+        # shape [num_sims, time_steps, num_agents, out_units]
 
-    # Skip connection.
-    msg_node = tf.concat([time_series, msg_aggregated], axis=-1)
-    # shape [num_sims, time_steps, num_edges, 2*ndims + out_units]
+        # Skip connection.
+        msg_node = tf.concat([time_series, msg_aggregated], axis=-1)
+        # shape [num_sims, time_steps, num_edges, 2*ndims + out_units]
 
     # MLP encoder
     msg_node_encoded = mlp_layers(msg_node,
                                   params['hidden_units'],
                                   params['dropout'],
                                   batch_norm=False,
-                                  training=training)
+                                  training=training,
+                                  name='node_state_MLP_decoder')
 
-    pred_displacement = tf.layers.dense(msg_node_encoded, ndims)
+    pred_displacement = tf.layers.dense(msg_node_encoded, ndims, name='linear')
 
     return time_series + pred_displacement
 
--- gnn/encoder.py
+++ gnn/encoder.py
@@ -7,20 +7,22 @@ from .utils import fc_matrix
 
 def node_to_edge(node_state, edge_sources, edge_targets):
     """Propagate node states to edges."""
-    msg_from_source = tf.transpose(tf.tensordot(node_state, edge_sources, axes=[[1], [1]]),
-                                   perm=[0, 2, 1])
-    msg_from_target = tf.transpose(tf.tensordot(node_state, edge_targets, axes=[[1], [1]]),
-                                   perm=[0, 2, 1])
-    ## msg_from_source and msg_from_target in shape [num_sims, num_edges, out_units]
-    msg_edge = tf.concat([msg_from_source, msg_from_target], axis=-1)
+    with tf.name_scope("node_to_edge"):
+        msg_from_source = tf.transpose(tf.tensordot(node_state, edge_sources, axes=[[1], [1]]),
+                                       perm=[0, 2, 1])
+        msg_from_target = tf.transpose(tf.tensordot(node_state, edge_targets, axes=[[1], [1]]),
+                                       perm=[0, 2, 1])
+        ## msg_from_source and msg_from_target in shape [num_sims, num_edges, out_units]
+        msg_edge = tf.concat([msg_from_source, msg_from_target], axis=-1)
 
     return msg_edge
 
 
 def edge_to_node(edge_msg, edge_targets):
     """Send edge messages to target nodes."""
-    node_state = tf.transpose(tf.tensordot(edge_msg, edge_targets, axes=[[1], [0]]),
-                              perm=[0, 2, 1])  # Shape [num_sims, num_agents, out_units].
+    with tf.name_scope("edge_to_node"):
+        node_state = tf.transpose(tf.tensordot(edge_msg, edge_targets, axes=[[1], [0]]),
+                                  perm=[0, 2, 1])  # Shape [num_sims, num_agents, out_units].
 
     return node_state
 
@@ -30,24 +32,27 @@ def mlp_encoder(features, classes, params, training=False):
     time_steps, num_agents, ndims = features.shape.as_list()[1:]
     # Input Layer
     # Transpose to [num_sims, num_agents, time_steps, ndims]
-    features = tf.transpose(features, [0, 2, 1, 3])
-    state = tf.reshape(features, shape=[-1, num_agents, time_steps * ndims])
+    with tf.name_scope("input_shape"):
+        features = tf.transpose(features, [0, 2, 1, 3])
+        state = tf.reshape(features, shape=[-1, num_agents, time_steps * ndims])
     # Node state encoder with MLP.
-    state = mlp_layers(state,
-                       params['hidden_units'],
-                       params['dropout'],
-                       params['batch_norm'],
-                       training=training)
+    node_state = mlp_layers(state,
+                            params['hidden_units'],
+                            params['dropout'],
+                            params['batch_norm'],
+                            training=training,
+                            name="node_encoding_MLP_1")
 
     # Send encoded state to edges.
     # `edge_sources` and `edge_targets` in shape [num_edges, num_agents].
     edge_sources, edge_targets = np.where(fc_matrix(num_agents))
     # One-hot representation of indices of edge sources and targets.
-    edge_sources = tf.one_hot(edge_sources, num_agents)
-    edge_targets = tf.one_hot(edge_targets, num_agents)
+    with tf.name_scope("one_hot"):
+        edge_sources = tf.one_hot(edge_sources, num_agents)
+        edge_targets = tf.one_hot(edge_targets, num_agents)
 
     # Form edges. Shape [num_sims, num_edges, 2 * hidden_units]
-    msg_edge = node_to_edge(state, edge_sources, edge_targets)
+    msg_edge = node_to_edge(node_state, edge_sources, edge_targets)
 
     # Store skip.
     msg_edge_skip = msg_edge
@@ -57,7 +62,8 @@ def mlp_encoder(features, classes, params, training=False):
                           params['hidden_units'],
                           params['dropout'],
                           params['batch_norm'],
-                          training=training)
+                          training=training,
+                          name='edge_encoding_MLP_1')
 
     # Compute edge influence to node. Shape [num_sims, num_agents, hidden_units]
     node_state = edge_to_node(msg_edge, edge_targets)
@@ -67,22 +73,25 @@ def mlp_encoder(features, classes, params, training=False):
                             params['hidden_units'],
                             params['dropout'],
                             params['batch_norm'],
-                            training=training)
+                            training=training,
+                            name='node_encoding_MLP_2')
 
     # Propagate node states to edges again.
-    msg_edge = node_to_edge(state, edge_sources, edge_targets)
+    msg_edge = node_to_edge(node_state, edge_sources, edge_targets)
 
     # Encode edge messages with MLP.
     msg_edge = mlp_layers(msg_edge,
                           params['hidden_units'],
                           params['dropout'],
                           params['batch_norm'],
-                          training=training)
+                          training=training,
+                          name='edge_encoding_MLP_2')
 
     # Concatenate skip msg. Shape [num_sims, num_edges, 4 * hidden_units]
     msg_edge = tf.concat([msg_edge, msg_edge_skip], axis=-1)
 
-    edge_type = tf.layers.dense(msg_edge, classes)  # Shape [num_sims, num_edges, classes]
+    # Shape [num_sims, num_edges, classes]
+    edge_type = tf.layers.dense(msg_edge, classes, name='edge_type_encoding')
 
     return edge_type
 
