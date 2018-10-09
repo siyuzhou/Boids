f57d05830618b6539efc787796a847e101568cb7
Siyu Zhou <siyu.zhou@outlook.com>
2018-10-08 23:12:49 -0700

    Separate `mlp_decoder_one_step`.

--- gnn/decoder.py
+++ gnn/decoder.py
@@ -5,7 +5,74 @@ from .modules import *
 from .utils import fc_matrix
 
 
-def mlp_decoder(features, params, pred_steps, training=False):
+def mlp_decoder_one_step(prev_step, edge_type, edge_sources, edge_targets, params, scope=None, training=False):
+    """Predict the next step."""
+    # prev_step shape [num_sims, time_steps, num_agents, ndims]
+    ndims = prev_step.shape.as_list()[-1]
+
+    num_types = edge_type.shape.as_list()[-1]
+    with tf.variable_scope(scope) as inner_scope:
+        with tf.name_scope(inner_scope.original_name_scope):
+            # Send encoded state to edges.
+            with tf.name_scope('node_to_edge'):
+                msg_from_source = tf.transpose(tf.tensordot(prev_step, edge_sources,
+                                                            axes=[[2], [1]]),
+                                               perm=[0, 1, 3, 2])
+                msg_from_target = tf.transpose(tf.tensordot(prev_step, edge_targets,
+                                                            axes=[[2], [1]]),
+                                               perm=[0, 1, 3, 2])
+                # msg_from_source and msg_from_target in shape [num_sims, time_steps, num_edges, ndims]
+                msg_edge = tf.concat([msg_from_source, msg_from_target], axis=-1)
+
+            encoded_msg_by_type = []
+            # Encode edge message by types and concatenate them.
+            start = 1 if params['skip_zero'] else 0
+            for i in range(start, num_types):
+                # mlp_encoder for one edge type.
+                encoded_msg = mlp_layers(msg_edge,
+                                         params['hidden_units'],
+                                         params['dropout'],
+                                         params['batch_norm'],
+                                         training=training,
+                                         name='edge_MLP_encoder_{}'.format(i))
+
+                encoded_msg_by_type.append(encoded_msg)
+
+            encoded_msg_by_type = tf.stack(encoded_msg_by_type, axis=3)
+            # shape [num_sims, time_steps, num_edges, num_types, out_units]
+
+            with tf.name_scope('edge_encoding_avg'):
+                # Sum of the edge encoding from all possible types.
+                encoded_msg_sum = tf.reduce_sum(tf.multiply(encoded_msg_by_type,
+                                                            edge_type[:, :, :, start:, :]),
+                                                axis=3)
+                # shape [num_sims, time_steps, num_edges, out_units]
+
+            # Aggregate msg from all edges to target node.
+            with tf.name_scope('edge_to_node'):
+                msg_aggregated = tf.transpose(tf.tensordot(encoded_msg_sum, edge_targets,
+                                                           axes=[[2], [0]]),
+                                              perm=[0, 1, 3, 2])
+                # shape [num_sims, time_steps, num_agents, out_units]
+
+                # Skip connection.
+                msg_node = tf.concat([prev_step, msg_aggregated], axis=-1)
+                # shape [num_sims, time_steps, num_edges, 2*ndims + out_units]
+
+            # MLP encoder
+            msg_node_encoded = mlp_layers(msg_node,
+                                          params['hidden_units'],
+                                          params['dropout'],
+                                          batch_norm=False,
+                                          training=training,
+                                          name='node_state_MLP_decoder')
+
+            pred_state = tf.layers.dense(msg_node_encoded, ndims, name='linear')
+
+            return pred_state
+
+
+def mlp_decoder_multisteps(features, params, pred_steps, training=False):
     time_series = features['time_series']
     # time_series shape [num_sims, time_steps, num_agents, ndims]
     # edge_type shape [num_sims, num_edges, num_edge_types]
@@ -20,7 +87,6 @@ def mlp_decoder(features, params, pred_steps, training=False):
 
     with tf.name_scope('edge_type'):
         edge_type = features['edge_type']
-        num_types = edge_type.shape.as_list()[-1]
         # Expand edge_type so that it has same number of dimensions as time_series.
         edge_type = tf.expand_dims(edge_type, 1)
         edge_type = tf.expand_dims(edge_type, 4)
@@ -33,72 +99,15 @@ def mlp_decoder(features, params, pred_steps, training=False):
         # num_starting_points == (time_steps + pred_steps - 1) // pred_steps
 
     def decoder_one_step(pred_time_series):
-        """Predict the next step."""
         prev_step = pred_time_series[:, :, -1, :, :]
-        with tf.variable_scope(scope) as inner_scope:
-            with tf.name_scope(inner_scope.original_name_scope):
-                # Send encoded state to edges.
-                with tf.name_scope('node_to_edge'):
-                    msg_from_source = tf.transpose(tf.tensordot(prev_step, edge_sources,
-                                                                axes=[[2], [1]]),
-                                                   perm=[0, 1, 3, 2])
-                    msg_from_target = tf.transpose(tf.tensordot(prev_step, edge_targets,
-                                                                axes=[[2], [1]]),
-                                                   perm=[0, 1, 3, 2])
-                    # msg_from_source and msg_from_target in shape [num_sims, time_steps, num_edges, ndims]
-                    msg_edge = tf.concat([msg_from_source, msg_from_target], axis=-1)
+        next_step = prev_step + \
+            mlp_decoder_one_step(prev_step, edge_type, edge_sources,
+                                 edge_targets, params, scope, training)
 
-                encoded_msg_by_type = []
-                # Encode edge message by types and concatenate them.
-                start = 1 if params['skip_zero'] else 0
-                for i in range(start, num_types):
-                    # mlp_encoder for one edge type.
-                    encoded_msg = mlp_layers(msg_edge,
-                                             params['hidden_units'],
-                                             params['dropout'],
-                                             params['batch_norm'],
-                                             training=training,
-                                             name='edge_MLP_encoder_{}'.format(i))
+        pred_time_series = tf.concat([pred_time_series, tf.expand_dims(next_step, 2)],
+                                     axis=2)
 
-                    encoded_msg_by_type.append(encoded_msg)
-
-                encoded_msg_by_type = tf.stack(encoded_msg_by_type, axis=3)
-                # shape [num_sims, time_steps, num_edges, num_types, out_units]
-
-                with tf.name_scope('edge_encoding_avg'):
-                    # Sum of the edge encoding from all possible types.
-                    encoded_msg_sum = tf.reduce_sum(tf.multiply(encoded_msg_by_type,
-                                                                edge_type[:, :, :, start:, :]),
-                                                    axis=3)
-                    # shape [num_sims, time_steps, num_edges, out_units]
-
-                # Aggregate msg from all edges to target node.
-                with tf.name_scope('edge_to_node'):
-                    msg_aggregated = tf.transpose(tf.tensordot(encoded_msg_sum, edge_targets,
-                                                               axes=[[2], [0]]),
-                                                  perm=[0, 1, 3, 2])
-                    # shape [num_sims, time_steps, num_agents, out_units]
-
-                    # Skip connection.
-                    msg_node = tf.concat([prev_step, msg_aggregated], axis=-1)
-                    # shape [num_sims, time_steps, num_edges, 2*ndims + out_units]
-
-                # MLP encoder
-                msg_node_encoded = mlp_layers(msg_node,
-                                              params['hidden_units'],
-                                              params['dropout'],
-                                              batch_norm=False,
-                                              training=training,
-                                              name='node_state_MLP_decoder')
-
-                pred_displacement = tf.layers.dense(msg_node_encoded, ndims, name='linear')
-
-                new_step = prev_step + pred_displacement
-
-                pred_time_series = tf.concat([pred_time_series, tf.expand_dims(new_step, 2)],
-                                             axis=2)
-
-                return pred_time_series
+        return pred_time_series
 
     i = 0
     _, pred_time_series = tf.while_loop(
@@ -119,5 +128,5 @@ def mlp_decoder(features, params, pred_steps, training=False):
 
 
 decoder_fn = {
-    'mlp': mlp_decoder
+    'mlp': mlp_decoder_multisteps
 }
