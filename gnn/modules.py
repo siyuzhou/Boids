9cce0fd7e89021702fe71c82362e9064a7b75492
Siyu Zhou <siyu.zhou@outlook.com>
2019-01-23 12:40:46 -0700

    Revert to using Relu.

--- gnn/modules.py
+++ gnn/modules.py
@@ -6,10 +6,10 @@ def mlp_layers(inputs, layers, dropout=0., batch_norm=False, training=False, nam
     with tf.variable_scope(name, default_name="MLP", reuse=tf.AUTO_REUSE):
         h = inputs
         for units in layers[:-1]:
-            h = tf.layers.dense(h, units, activation=tf.nn.elu)
+            h = tf.layers.dense(h, units, activation=tf.nn.relu)
             h = tf.layers.dropout(h, dropout, training=training)
 
-        output = tf.layers.dense(h, layers[-1], activation=tf.nn.elu)
+        output = tf.layers.dense(h, layers[-1], activation=tf.nn.relu)
         # shape [num_sims, time_steps, num_edges, out_units]
 
         if batch_norm:
